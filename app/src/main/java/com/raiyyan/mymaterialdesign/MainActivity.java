74e2c408976cce58389daa948b7a267ec061b44c
Zakir Hussain <kirzahussain@gmail.com>
2015-08-31 14:16:39 +0530

    Implemented design support library's Toolbar, Coordinator layout, and Floating Action button

--- app/src/main/java/com/raiyyan/mymaterialdesign/MainActivity.java
+++ app/src/main/java/com/raiyyan/mymaterialdesign/MainActivity.java
@@ -1,14 +1,39 @@
 package com.raiyyan.mymaterialdesign;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Hello", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).show();
+            }
+        });
+
     }
 
 
